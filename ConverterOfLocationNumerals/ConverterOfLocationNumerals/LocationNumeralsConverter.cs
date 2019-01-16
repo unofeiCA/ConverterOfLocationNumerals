43a2bc76451f04abcfa1454c871d42661ed11814
yufei <unofei@gmail.com>
2019-01-15 22:29:57 -0600

    convert between integer and location numerals, first commit, almost done, need tweak and test

---
+++ ConverterOfLocationNumerals/ConverterOfLocationNumerals/LocationNumeralsConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace ConverterOfLocationNumerals
+{
+    public class LocationNumeralsConverter
+    {
+        static readonly char[] Letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        const char CONNECTOR = '-';
+        const int POWER_2_26 = 67108864;
+
+        public static string ConverIntegerToLocationNumerals(int num) 
+        {
+            string ret = "";
+            int temp = num;
+            int counter = 0;
+            while (temp > 0) 
+            {
+                string tempRet = "";
+                for (int i = 0; i < Letters.Length; i++)
+                {
+                    if ((temp & 0x01) != 0)
+                    {
+                        tempRet += Letters[i];
+                    }
+                    temp = temp >> 1;
+                }
+                if (counter > 0)
+                {
+                    ret = CONNECTOR + ret;
+                }
+                ret = tempRet + ret;
+                counter++;
+            }
+
+            return ret;
+        }
+
+        public static int ConverLocationNumeralsToInteger(string locationNum)
+        {
+            string[] nums = locationNum.Split(CONNECTOR);
+            int ret = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ret += (int)Math.Pow(POWER_2_26, nums.Length - 1 - i) * LocationNumeralToInteger(nums[i]);
+            }
+            return 0;
+        }
+
+        public static string ConverLocationNumeralsToAbbreviation(string locationNum)
+        {
+            return ConverIntegerToLocationNumerals(ConverLocationNumeralsToInteger(locationNum));
+
+        }
+
+        private static int LocationNumeralToInteger(string locationNum) 
+        {
+            int ret = 0;
+            for (int i = 0; i < locationNum.Length; i++)
+            {
+                int index = Array.IndexOf(Letters, locationNum[i]);
+                ret += (int)Math.Pow(2, index);
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ ConverterOfLocationNumerals/ConverterOfLocationNumerals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConverterOfLocationNumerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a positive number. Negative number and 0 are not supported.");
+            int number = 0;
+            Int32.TryParse(Console.ReadLine(), out number);
+            if (number <= 0)
+            {
+                Console.WriteLine("Please enter a positive number. Negative number and 0 are not supported.");
+
+            }
+            else
+            {
+                Console.WriteLine(LocationNumeralsConverter.ConverIntegerToLocationNumerals(number));
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
